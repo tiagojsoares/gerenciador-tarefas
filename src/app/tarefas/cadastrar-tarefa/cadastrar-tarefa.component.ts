c5fc94a72a9d667c883c5544139800aef0bd0606
tiagojsoares <tiagoedah@live.com>
2020-03-21 19:29:11 -0300

    1 file modified, 1 added

--- src/app/tarefas/cadastrar-tarefa/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar-tarefa/cadastrar-tarefa.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from "@angular/core";
+import { Tarefa } from "../shared";
+import { NgForm } from "@angular/forms";
+
 
 @Component({
-  selector: 'app-cadastrar-tarefa',
-  templateUrl: './cadastrar-tarefa.component.html',
-  styleUrls: ['./cadastrar-tarefa.component.css']
+  selector: "app-cadastrar-tarefa",
+  templateUrl: "./cadastrar-tarefa.component.html",
+  styleUrls: ["./cadastrar-tarefa.component.css"]
 })
 export class CadastrarTarefaComponent implements OnInit {
+  @ViewChild("formTarefa", { static: true }) formTarefa: NgForm;
+  tarefa:Tarefa;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
