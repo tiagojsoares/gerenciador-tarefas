927235595f41d0eaa03d0eb72fdf48358b459771
tiagojsoares <tiagoedah@live.com>
2020-03-21 16:23:59 -0300

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {AppRoutingModule} from './app-routing.module';
+import {TarefasModule} from './tarefas';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,NgbModule,AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule,NgbModule,AppRoutingModule,TarefasModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
