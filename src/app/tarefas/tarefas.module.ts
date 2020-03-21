9aa14008f58f5ec608e46decac5895f3b0952aed
tiagojsoares <tiagoedah@live.com>
2020-03-21 16:36:12 -0300

    6 files added

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+
+
+@NgModule({
+  imports: [CommonModule, RouterModule, FormsModule],
+  declarations: [
+  ],
+  providers: []
+})
+export class TarefasModule {}
