e91a0dce784c230d90cad76a0faa571d1132c39b
tiagojsoares <tiagoedah@live.com>
2020-03-21 17:54:28 -0300

    1 file modified

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
-import { TarefaService } from './shared';
-import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
-
+import { TarefaService } from "./shared";
+import { ListarTarefaComponent } from "./listar-tarefa/";
 
 @NgModule({
   imports: [CommonModule, RouterModule, FormsModule],
-  declarations: [
-  ListarTarefaComponent],
+  declarations: [ListarTarefaComponent],
   providers: [TarefaService]
 })
 export class TarefasModule {}
