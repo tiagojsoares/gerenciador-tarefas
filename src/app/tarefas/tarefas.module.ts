79aba547ed9c13b36294356e2d87137512220c4d
tiagojsoares <tiagoedah@live.com>
2020-03-21 17:43:28 -0300

    criado componte listar tarefa

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
 
 
 @NgModule({
   imports: [CommonModule, RouterModule, FormsModule],
   declarations: [
-  ],
+  ListarTarefaComponent],
   providers: [TarefaService]
 })
 export class TarefasModule {}
