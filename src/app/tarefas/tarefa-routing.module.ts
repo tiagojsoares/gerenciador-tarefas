91d7e0ca30a4f76322a333a2a6dd753c97ddd3e6
tiagojsoares <tiagoedah@live.com>
2020-03-21 19:05:17 -0300

    1 file modified

--- src/app/tarefas/tarefa-routing.module.ts
+++ src/app/tarefas/tarefa-routing.module.ts
@@ -1,17 +1,20 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-import { ListarTarefaComponent } from './listar-tarefa';
-//import { CadastrarTarefaComponent } from './cadastrar';
+import { ListarTarefaComponent } from "./listar-tarefa";
+import { CadastrarTarefaComponent } from "./cadastrar";
 //import { EditarTarefaComponent } from './editar';
 
 export const TarefaRoutes: Routes = [
-	{ 
-		path: 'tarefas', 
-		redirectTo: 'tarefas/listar' 
-	},
-	{ 
-		path: 'tarefas/listar', 
-		component: ListarTarefaComponent 
-	}
-
+  {
+    path: "tarefas",
+    redirectTo: "tarefas/listar"
+  },
+  {
+    path: "tarefas/listar",
+    component: ListarTarefaComponent
+  },
+  {
+    path: "tarefas/cadastrar",
+    component: CadastrarTarefaComponent
+  }
 ];
